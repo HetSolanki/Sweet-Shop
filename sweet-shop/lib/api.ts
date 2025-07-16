b3afa443b02ad02df6d2a97a7d78b6ef856f1573
H.$ <het.solanki109@gmail.com>
2025-07-17 00:42:31 +0530

    feat: implement add-sweet feature for frontend design

--- sweet-shop/lib/api.ts
+++ sweet-shop/lib/api.ts
@@ -1,6 +1,7 @@
 "use server";
-
+  
 import { prisma } from "./prisma";
+import { AddSweetType } from "@/types/sweetApiTypes";
 
 export const fetchAllSweets = async () => {
   const res = await fetch("http://localhost:3000/api/sweets");
@@ -48,4 +49,25 @@ export const deleteSweet = async (sweetId: string) => {
 
   const results = await res.json();
   return results;
+};
+
+export const addSweet = async ({
+  name,
+  categoryId,
+  price,
+  quantity,
+}: AddSweetType) => {
+  const body = {
+    name,
+    categoryId,
+    price,
+    quantity,
+  };
+  const res = await fetch("http://localhost:3000/api/sweets/add", {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+
+  const result = await res.json();
+  return result;
 };

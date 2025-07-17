c5a010466a47931df6ba7cd5aaf9e3caf36405e3
HetSolanki <het.solanki721@gmail.com>
2025-07-17 16:37:59 +0530

    fix: fixed some input validations

--- sweet-shop/components/SweetCard.tsx
+++ sweet-shop/components/SweetCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Sweet } from "@/types/sweetTypes";
 import Popup from "./popover";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
--- sweet-shop/components/restock.tsx
+++ sweet-shop/components/restock.tsx
@@ -3,19 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { purchaseSweet, restockSweet } from "@/lib/api";
+import { restockSweet } from "@/lib/api";
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { toast } from "sonner";
-import { AlertTriangle } from "lucide-react";
-import { useRouter } from "next/navigation";
 
 export default function Restock({ sweetId }: { sweetId: string }) {
   const [quantity, setQuantity] = useState("");
   const [open, setOpen] = useState(false);
-  const router = useRouter();
 
-  const handlePurchase = async () => {
+  const handleRestock = async (e: React.FormEvent) => {
+    e.preventDefault();
     const res = await restockSweet(sweetId, quantity);
 
     if (res.status === 200) {
@@ -23,17 +21,7 @@ export default function Restock({ sweetId }: { sweetId: string }) {
       setOpen(false);
       location.reload();
     } else {
-      toast.custom(() => (
-        <div
-          className="w-full max-w-sm bg-red-500 border border-destructive rounded-md shadow-lg p-4 flex items-start gap-3"
-          role="alert"
-        >
-          <AlertTriangle className="text-white h-5 w-5" />
-          <div className="">
-            <p className="text-sm text-white">{res.error}</p>
-          </div>
-        </div>
-      ));
+      toast.info(res.error);
     }
   };
 
@@ -44,29 +32,32 @@ export default function Restock({ sweetId }: { sweetId: string }) {
       </DialogTrigger>
 
       <DialogContent className="w-80">
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="leading-none font-medium">Quantity</h4>
-            <p className="text-muted-foreground text-sm">
-              Enter quantity to purchase
-            </p>
-          </div>
+        <form onSubmit={handleRestock}>
+          <div className="grid gap-4">
+            <div className="space-y-2">
+              <h4 className="leading-none font-medium">Quantity</h4>
+              <p className="text-muted-foreground text-sm">
+                Enter quantity to restock
+              </p>
+            </div>
 
-          <div className="grid grid-cols-3 items-center gap-4">
-            <Label htmlFor="qty">Qty</Label>
-            <Input
-              id="qty"
-              type="number"
-              min={1}
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-              className="col-span-2 h-8"
-              placeholder="e.g. 2"
-            />
-          </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="qty">Qty</Label>
+              <Input
+                id="qty"
+                type="number"
+                min={1}
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                className="col-span-2 h-8"
+                placeholder="e.g. 2"
+                required
+              />
+            </div>
 
-          <Button onClick={handlePurchase}>Restock</Button>
-        </div>
+            <Button type="submit">Restock</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );

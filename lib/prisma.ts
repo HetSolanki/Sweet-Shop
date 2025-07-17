2e9360ffb231f225f31a6252c9ad1653549880ac
H.$ <het.solanki109@gmail.com>
2025-07-18 00:32:30 +0530

    fix(config): update environment and Prisma settings for deployment compatibility

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
 // This file sets up a global PrismaClient instance for database access
 
 import { PrismaClient } from "@prisma/client";
-
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaLibSQL({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
